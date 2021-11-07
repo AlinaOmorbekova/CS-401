d88ccd7d7deca0381ca19cffca122fdd82398686
Alina <46772913+AlinaOmorbekova@users.noreply.github.com>
2021-11-08 00:31:07 +0600

    lab6

---
+++ Lab6/ListBox/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user_input = textBox1.Text;
+            listBox1.Items.Insert(0, user_input);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectionMode = SelectionMode.MultiSimple;
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            string message = "Вы действительно хотите очистить весь список?";
+            string title = "Очистить список";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                listBox1.Items.Clear();
+            }
+            else
+            {
+                listBox1 = listBox1;
+            }
+            
+        }
+    }
+}
