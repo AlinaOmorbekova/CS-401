803ac694755a0d25fb31f2c1b5d514b729982d87
Alina Omorbekova <alina.omorbekova@iaau.edu.kg>
2022-01-05 13:46:39 +0600

    lab-10

---
+++ Lab-10/Lab-10/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Lab_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.перваяФормаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.secondFormToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.displayStyleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.arrangeIconsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cascadeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.titleHorizontalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.titleVerticalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.calculationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.перваяФормаToolStripMenuItem,
+            this.secondFormToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // перваяФормаToolStripMenuItem
+            // 
+            this.перваяФормаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.displayStyleToolStripMenuItem});
+            this.перваяФормаToolStripMenuItem.Name = "перваяФормаToolStripMenuItem";
+            this.перваяФормаToolStripMenuItem.Size = new System.Drawing.Size(105, 29);
+            this.перваяФормаToolStripMenuItem.Text = "First form";
+            // 
+            // secondFormToolStripMenuItem
+            // 
+            this.secondFormToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.calculationToolStripMenuItem});
+            this.secondFormToolStripMenuItem.Name = "secondFormToolStripMenuItem";
+            this.secondFormToolStripMenuItem.Size = new System.Drawing.Size(131, 29);
+            this.secondFormToolStripMenuItem.Text = "Second form";
+            // 
+            // displayStyleToolStripMenuItem
+            // 
+            this.displayStyleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arrangeIconsToolStripMenuItem,
+            this.cascadeToolStripMenuItem,
+            this.titleHorizontalToolStripMenuItem,
+            this.titleVerticalToolStripMenuItem});
+            this.displayStyleToolStripMenuItem.Name = "displayStyleToolStripMenuItem";
+            this.displayStyleToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.displayStyleToolStripMenuItem.Text = "Display style";
+            // 
+            // arrangeIconsToolStripMenuItem
+            // 
+            this.arrangeIconsToolStripMenuItem.Name = "arrangeIconsToolStripMenuItem";
+            this.arrangeIconsToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.arrangeIconsToolStripMenuItem.Text = "ArrangeIcons";
+            this.arrangeIconsToolStripMenuItem.Click += new System.EventHandler(this.arrangeIconsToolStripMenuItem_Click);
+            // 
+            // cascadeToolStripMenuItem
+            // 
+            this.cascadeToolStripMenuItem.Name = "cascadeToolStripMenuItem";
+            this.cascadeToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.cascadeToolStripMenuItem.Text = "Cascade";
+            // 
+            // titleHorizontalToolStripMenuItem
+            // 
+            this.titleHorizontalToolStripMenuItem.Name = "titleHorizontalToolStripMenuItem";
+            this.titleHorizontalToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.titleHorizontalToolStripMenuItem.Text = "TitleHorizontal";
+            // 
+            // titleVerticalToolStripMenuItem
+            // 
+            this.titleVerticalToolStripMenuItem.Name = "titleVerticalToolStripMenuItem";
+            this.titleVerticalToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.titleVerticalToolStripMenuItem.Text = "TitleVertical";
+            // 
+            // calculationToolStripMenuItem
+            // 
+            this.calculationToolStripMenuItem.Name = "calculationToolStripMenuItem";
+            this.calculationToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.calculationToolStripMenuItem.Text = "Calculation";
+            this.calculationToolStripMenuItem.Click += new System.EventHandler(this.calculationToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem перваяФормаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem displayStyleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem arrangeIconsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cascadeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem secondFormToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem titleHorizontalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem titleVerticalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem calculationToolStripMenuItem;
+    }
+}
+
---
+++ Lab-10/Lab-10/Form3.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Lab_10
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Product = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.unit_price = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Quantity = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Sum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "milk",
+            "bread",
+            "coffee",
+            "sugar"});
+            this.comboBox1.Location = new System.Drawing.Point(39, 56);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(158, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(253, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(427, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 26);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(608, 58);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 26);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "product";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(249, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "unit price";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(423, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "quantity";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(604, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "sum";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(307, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 33);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Add to check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Product,
+            this.unit_price,
+            this.Quantity,
+            this.Sum});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(39, 183);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(694, 173);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(608, 400);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(125, 26);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(534, 400);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Total";
+            // 
+            // Product
+            // 
+            this.Product.Text = "Product";
+            this.Product.Width = 100;
+            // 
+            // unit_price
+            // 
+            this.unit_price.Text = "Unit price";
+            this.unit_price.Width = 100;
+            // 
+            // Quantity
+            // 
+            this.Quantity.Text = "Quantity";
+            this.Quantity.Width = 100;
+            // 
+            // Sum
+            // 
+            this.Sum.Text = "Sum";
+            this.Sum.Width = 100;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Product;
+        private System.Windows.Forms.ColumnHeader unit_price;
+        private System.Windows.Forms.ColumnHeader Quantity;
+        private System.Windows.Forms.ColumnHeader Sum;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+    }
+}
